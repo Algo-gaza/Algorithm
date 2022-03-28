20bd06ae19be05b9cd29f22730193bf4360ddfc2
ySeungJE <98517187+ySeungJE@users.noreply.github.com>
2022-03-28 16:36:41 +0900

    [ 🦅 ADD ] : 튜플

---
+++ 220328/튜플/seungje.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public static List<Integer> solution(String s) {
+        StringTokenizer st = new StringTokenizer(s, "{,}");
+        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map2 = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+      
+        while(st.hasMoreTokens()) {
+            int t = Integer.parseInt(st.nextToken());
+            map.put(t, map.getOrDefault(t, 0) + 1);
+        }
+      
+        for(Integer i : map.keySet()) 
+            map2.put(map.get(i), i);
+      
+        for(int i=map.size(); i>=1; i--) 
+            list.add(map2.get(i));
+      
+        return list;
+    }
+}
